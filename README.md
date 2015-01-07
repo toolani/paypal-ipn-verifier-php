60a389860bfa39aed704df1510f2847772af078c
pthompson <peter.thompson@toolani.com>
2015-01-07 14:39:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,144 @@
+PayPal IPN Verifier
+===================
+
+A PayPal Instant Payment Notification (IPN) verifier class for PHP 5. 
+
+Use the `IpnVerifier` class in your IPN script to handle the encoding 
+of POST data, post back to PayPal, and parsing of the response from PayPal.
+
+This class was based on (and is almost API-compatible with) [Quixotix/PHP-PayPal-IPN][1]
+
+[1]: https://github.com/Quixotix/PHP-PayPal-IPN
+
+Features
+--------
+
+- Supports live and sandbox environments.
+- Verifies an HTTP &quot;200&quot; response status code from the PayPal server.
+- Get detailed plain text reports of the entire IPN using the `getTextReport()`
+  method for use in emails and logs to administrators.
+- Throws various exceptions to differentiate between common errors in code or
+  server configuration versus invalid IPN responses.
+  
+Differences from Quixotix/PHP-PayPal-IPN
+----------------------------------------
+
+- Always verifies IPNs using SSL.
+- Always uses cURL (no fsockopen support).
+- PSR-2 compliant coding style.
+
+Getting Started
+---------------
+
+This code is intended for web developers. You should understand how the IPN
+process works conceptually and you should understand when and why you would be
+using IPN. Reading the [PayPal Instant Payment Notification Guide][1] is a good
+place to start.
+
+You should also have a [PayPal Sandbox Account][2] with a test buyer account and a test seller account. When logged into your sandbox account there is an IPN simulator under the 'Test Tools' menu which you can used to test your IPN 
+listener.
+
+[2]: https://cms.paypal.com/cms_content/US/en_US/files/developer/IPNGuide.pdf
+[3]: https://developer.paypal.com
+
+Once you have your sandbox account set up, you simply create a PHP script that will be your IPN listener. In that script, use the `IpnVerifier()` class as shown below.
+
+```
+<?php
+use Toolani\Payment\Paypal\IpnVerifier;
+
+// If you're doing this for real, autoloading would be better
+include("/path/to/Toolani/Payment/Paypal/IpnVerifier.php");
+
+$verifier = new IpnVerifier($useSandbox = true);
+
+try {
+    $verified = $verifier->verify($_POST);
+} catch (Exception $e) {
+    // fatal error trying to process IPN.
+    exit(1);
+}
+
+if ($verified) {
+    // IPN response was "VERIFIED"
+} else {
+    // IPN response was "INVALID"
+}
+
+```
+
+Quixotix/PHP-PayPal-IPN compatibility
+-------------------------------------
+
+It should be straightforward to port scripts using the PHP-PayPal-IPN class to use `IpnVerifier` instead, and a `processIpn()` method is provided to ease this.
+
+The usage example above is directly equivalent to the [example][4] given in the PHP-PayPal-IPN documentation.
+
+Things to be aware of:
+
+- Sandbox configuration is now done via a constructor argument, rather than a public property, though the `useSandbox` property remains public (for the moment).
+- There are no options for configuring cURL, SSL will always be used.
+- The `verify` method requires that you explicitly provide the array of POSTed data. PHP-PayPal-IPN's `processIpn` did not and would automagically use `$_POST` if given no data. A `processIpn` method is thus provided, which mimics the old behaviour. **Note**: `verify` is the preferred method for verifying IPNs and `processIpn` should be considered deprecated and may be removed in future versions of this class.
+
+[4]: https://github.com/Quixotix/PHP-PayPal-IPN/blob/master/README.md#getting-started
+
+
+Example Report
+--------------
+
+Here is an example of a report returned by the `getTextReport()` method. Create your own reports by extending the `IpnVerifier` class or by accessing the data directly in your ipn script.
+
+    --------------------------------------------------------------------------------
+    [09/09/2011 8:35 AM] - https://www.sandbox.paypal.com/cgi-bin/webscr (curl)
+    --------------------------------------------------------------------------------
+    HTTP/1.1 200 OK
+    Date: Fri, 09 Sep 2011 13:35:39 GMT
+    Server: Apache
+    X-Frame-Options: SAMEORIGIN
+    Set-Cookie: c9MWDuvPtT9GIMyPc3jwol1VSlO=Ch-NORlHUjlmbEm__KG9LupR4mfMfQTkx1QQ6hHDyc0RImWr88NY_ILeICENiwtVX3iw4jEnT1-1gccYjQafWrQCkDmiykNT8TeDUg7R7L0D9bQm47PTG8MafmrpyrUAxQfst0%7c_jG1ZL6CffJgwrC-stQeqni04tKaYSIZqyqhFU7tKnV520wiYOw0hwk5Ehrh3hLDvBxkpm%7cYTFdl0w0YpEqxu0D1jDTVTlEGXlmLs4wob2Glu9htpZkFV9O2aCyfQ4CvA2kLJmlI6YiXm%7c1315575340; domain=.paypal.com; path=/; Secure; HttpOnly
+    Set-Cookie: cookie_check=yes; expires=Mon, 06-Sep-2021 13:35:40 GMT; domain=.paypal.com; path=/; Secure; HttpOnly
+    Set-Cookie: navcmd=_notify-validate; domain=.paypal.com; path=/; Secure; HttpOnly
+    Set-Cookie: navlns=0.0; expires=Thu, 04-Sep-2031 13:35:40 GMT; domain=.paypal.com; path=/; Secure; HttpOnly
+    Set-Cookie: Apache=10.72.109.11.1315575339707456; path=/; expires=Sun, 01-Sep-41 13:35:39 GMT
+    X-Cnection: close
+    Transfer-Encoding: chunked
+    Content-Type: text/html; charset=UTF-8
+
+    VERIFIED
+    --------------------------------------------------------------------------------
+    test_ipn                 1
+    payment_type             instant
+    payment_date             06:34:51 Sep 09, 2011 PDT
+    payment_status           Completed
+    address_status           confirmed
+    payer_status             verified
+    first_name               John
+    last_name                Smith
+    payer_email              buyer@paypalsandbox.com
+    payer_id                 TESTBUYERID01
+    address_name             John Smith
+    address_country          United States
+    address_country_code     US
+    address_zip              95131
+    address_state            CA
+    address_city             San Jose
+    address_street           123, any street
+    business                 seller@paypalsandbox.com
+    receiver_email           seller@paypalsandbox.com
+    receiver_id              TESTSELLERID1
+    residence_country        US
+    item_name                something
+    item_number              AK-1234
+    quantity                 1
+    shipping                 3.04
+    tax                      2.02
+    mc_currency              USD
+    mc_fee                   0.44
+    mc_gross                 12.34
+    mc_gross_1               9.34
+    txn_type                 web_accept
+    txn_id                   51991334
+    notify_version           2.1
+    custom                   xyz123
+    charset                  windows-1252
+    verify_sign              Ah5rOpfPGo5g6FNg95DMPybP51J5AUEdXS1hqyRAP6WYYwaixKNDgQRR
---
+++ src/Toolani/Payment/Paypal/IpnVerifier.php
@@ -0,0 +1,236 @@
+<?php
+
+/*
+ *
+ * Copyright (c) 2015, toolani
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Toolani\Payment\Paypal;
+
+/**
+ * Verifies PayPal IPN data with PayPal.
+ */
+class IpnVerifier
+{    
+    /**
+     *  If true, the paypal sandbox URI www.sandbox.paypal.com is used for the
+     *  post back. If false, the live URI www.paypal.com is used. Default false.
+     */
+    public $useSandbox;
+    
+    /**
+     *  The amount of time, in seconds, to wait for the PayPal server to respond
+     *  before timing out. Default 30 seconds.
+     */
+    public $timeout = 30;
+    
+    private $postData       = array();
+    private $postUri        = '';     
+    private $responseStatus = '';
+    private $response       = '';
+
+    const PAYPAL_HOST  = 'www.paypal.com';
+    const SANDBOX_HOST = 'www.sandbox.paypal.com';
+    
+    /**
+     * @param boolean $useSandbox If true, IPNs will be verified using the PayPal sandbox, rather than the live URL.
+     */
+    public function __construct($useSandbox)
+    {
+        $this->useSandbox = $useSandbox;
+    }
+    
+    /**
+     *  Verifies IPN data with PayPal.
+     *
+     *  Handles the IPN post back to PayPal and parsing the response. Call this
+     *  method from your IPN listener script.
+     *  
+     *  Throws an exception if there is an error.
+     *
+     *  @param  array   $postData  POST variables as a key/value array
+     *  @return boolean            True if the response came back as "VERIFIED", false if the response came back "INVALID"
+     */ 
+    public function verify($postData)
+    {
+        if ($postData === null || empty($postData)) {
+            throw new \Exception("No POST data found.", 103);
+        }
+        
+        $encodedData    = 'cmd=_notify-validate';
+        $this->postData = $postData;
+            
+        foreach ($this->postData as $key => $value) {
+            $encodedData .= "&$key=".urlencode($value);
+        }
+        
+        // parent::debug('encoded_data:'.$encodedData);
+
+        $this->curlPost($encodedData);
+        
+        if (strpos($this->responseStatus, '200') === false) {
+            throw new \Exception("Invalid response status: ".$this->responseStatus, 104);
+        }
+        
+        if (strpos($this->response, "VERIFIED") !== false) {
+            return true;
+        } elseif (strpos($this->response, "INVALID") !== false) {
+            return false;
+        } else {
+            // parent::error("Unexpected response from PayPal.");
+            throw new \Exception("Unexpected response from PayPal.", 105);
+        }
+    }
+    
+    /**
+     *  Process IPN
+     *
+     *  An alias for 'verify', provided for compatibility with 
+     *  https://github.com/Quixotix/PHP-PayPal-IPN.
+     *  
+     *  Note that while this method will fall back to using $_POST if given no
+     *  $postData, 'verify' will NOT.
+     *
+     *  @param  array   $postData  POST variables as a key/value array, $_POST will be used directly if not provided
+     *  @return boolean            True if the response came back as "VERIFIED", false if the response came back "INVALID"
+     */    
+    public function processIpn($postData = null)
+    {
+        if ($postData === null) {
+            $postData = $_POST;
+        }
+        
+        return $this->verify($postData);
+    }
+    
+    /**
+     *  Get Text Report
+     *
+     *  This is useful in emails to order processors and system administrators.
+     *  Override this method in your own class to customize the report.
+     *
+     *  @return string A report of the IPN transaction in plain text format
+     */
+    public function getTextReport()
+    {
+        $r = '';
+        
+        // date and POST url
+        for ($i=0; $i<80; $i++) { $r .= '-'; }
+        $r .= "\n[".date('m/d/Y g:i A').'] - '.$this->getPostUri();
+        $r .= " (curl)\n";
+        
+        // HTTP Response
+        for ($i=0; $i<80; $i++) { $r .= '-'; }
+        $r .= "\n{$this->getResponse()}\n";
+        
+        // POST vars
+        for ($i=0; $i<80; $i++) { $r .= '-'; }
+        $r .= "\n";
+        
+        foreach ($this->postData as $key => $value) {
+            $r .= str_pad($key, 25)."$value\n";
+        }
+        $r .= "\n\n";
+        
+        return $r;
+    }
+
+    /**
+     *  Get POST URI
+     *
+     *  This can be useful for troubleshooting connection problems. The default URI
+     *  would be "ssl://www.sandbox.paypal.com:443/cgi-bin/webscr".
+     *
+     *  @return string The URI that was used to send the post data back to PayPal
+     */
+    public function getPostUri()
+    {
+        return $this->postUri;
+    }
+    
+    /**
+     *  Get Response
+     *
+     *  @return string The entire response from PayPal as a string including all the
+     *  HTTP headers
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+    
+    /**
+     *  Get Response Status
+     *
+     *  This should be "200" if the post back was successful.
+     *
+     *  @return string The HTTP response status code from PayPal 
+     */
+    public function getResponseStatus()
+    {
+        return $this->responseStatus;
+    }
+
+    /**
+     *  Post Back Using cURL
+     *
+     *  Sends the post back to PayPal using the cURL library. Called by
+     *  the processIpn() method. Throws an exception if the post fails. 
+     *  Populates the response, responseStatus, and postUri properties on 
+     *  success.
+     *
+     *  @param string The post data as a URL encoded string
+     */
+    protected function curlPost($encodedData)
+    {
+        $this->postUri = 'https://'.$this->getPaypalHost().'/cgi-bin/webscr';
+
+        // parent::debug('curlPost uri:'.$this->postUri);
+        
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__)."/../../../../cert/api_cert_chain.crt");
+        curl_setopt($ch, CURLOPT_URL, $this->postUri);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $encodedData);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        
+        // Ref: http://stackoverflow.com/questions/26378351/error1408f10bssl-routinesssl3-get-recordwrong-version-number-paypal-maybe
+        curl_setopt($ch, CURLOPT_SSLVERSION, 1);
+        
+        $this->response = curl_exec($ch);
+        $this->responseStatus = strval(curl_getinfo($ch, CURLINFO_HTTP_CODE));
+        
+        if ($this->response === false || $this->responseStatus == '0') {
+            $errno = curl_errno($ch);
+            $errstr = curl_error($ch);
+            
+            // parent::error("curl error, errno:$errno; errstr:$errstr");
+            
+            if($errno == 28) {
+                // cURL timeout error
+                throw new \Exception("cURL timeout error: [$errno] $errstr", 101);
+            } else {
+                throw new \Exception("cURL error: [$errno] $errstr", 100);
+            }
+        }
+    }
+    
+    private function getPaypalHost()
+    {
+        if ($this->useSandbox) {
+            return self::SANDBOX_HOST;
+        } else {
+            return self::PAYPAL_HOST;  
+        }
+    }
+}
